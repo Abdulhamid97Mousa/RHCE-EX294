c7fee2a1521f7636998eaae6765de066861971a1
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2022-12-05 21:02:11 +0800

    remove K8s collection

--- Advanced_Ansible_Automation/Module4/collections/ansible_collections/kubernetes/core/tests/integration/targets/inventory_k8s/runme.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-export ANSIBLE_INVENTORY_ENABLED=kubernetes.core.k8s,yaml
-export ANSIBLE_PYTHON_INTERPRETER=auto_silent
-
-ansible-playbook playbooks/play.yml -i playbooks/test.inventory_k8s.yml "$@"
