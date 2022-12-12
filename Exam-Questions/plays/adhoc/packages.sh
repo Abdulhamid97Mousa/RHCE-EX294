a182d8dbfbbb574684100c787e55535639ef7fd2
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2022-12-12 08:40:52 +0800

    Add files

--- Exam-Questions/plays/adhoc/packages.sh
+++ Exam-Questions/plays/adhoc/packages.sh
@@ -5,7 +5,7 @@ ansible localhost -m file -a 'path=/home/automation/.ssh state=directory'
 # Generate the keypair
 ansible localhost -m openssh_keypair -a "path=/home/automation/.ssh/id_rsa owner=automation group=automation type=rsa"
 # Create the user, calling it with sudo will avoid host key checking
-ansible all -b -m user -a "name=automation password={{ 'devops' | password_hash('sha512') }}"
+ansible all -b -m user -a "name=automation password={{ 'devops' | password_hash('sha512') }} state=present"
 # Share public key with rest of the nodes, inventory is taken from the previous exercise
 ansible all -b -m authorized_key -a "key={{ lookup('file', '/home/automation/.ssh/id_rsa.pub') }} user=automation state=present"
 # Allow passwordless sudo
