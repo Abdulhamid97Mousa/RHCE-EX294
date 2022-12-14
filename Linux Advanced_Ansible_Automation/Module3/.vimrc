245f0cadbbd77a075dbba8ab664ee1ca78e1efbf
Abdulhamid97Mousa <80536675+Abdulhamid97Mousa@users.noreply.github.com>
2022-12-15 06:55:16 +0800

    Fix Submodules

--- Linux Advanced_Ansible_Automation/Module3/.vimrc
+++
@@ -1,3 +0,0 @@
-syntax on
-set bg=dark
-autocmd FileType yaml setlocal ai et ts=2 sw=2 cuc cul
--- Linux Advanced_Ansible_Automation/Module5/roles/mysql/files/demo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS demo (
-   message varchar(255) NOT NULL
-)  ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO demo (message) VALUES('Hello World');
